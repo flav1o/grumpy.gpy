d731ca62b56470439c02e1df70dc172f3f0144f9
flav1o <flaviodanielmartinscosta@gmail.com>
2022-10-07 23:07:13 +0100

    add ints and decimals numbers

--- parse.ts
+++ parse.ts
@@ -4,7 +4,7 @@ const grammar = require("./grumpy.js");
 const parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar));
 
 try {
-	parser.feed("helloHolder->hello");
+	parser.feed("var->12.3");
 	console.log(parser.results);
 } catch (e) {
 	console.log("error => ", e.message);
